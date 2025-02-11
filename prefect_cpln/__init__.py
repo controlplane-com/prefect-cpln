9878f37002737aa1cec2766a1a8a3471f9799ed5
MajidAbuRmila <majid@controlplane.com>
2025-02-11 22:21:17 +0200

    Include CplnClient back to init file

--- prefect_cpln/__init__.py
+++ prefect_cpln/__init__.py
@@ -1,5 +1,9 @@
 from . import _version
-from prefect_cpln.credentials import CplnCredentials, CplnConfig  # noqa F401
+from prefect_cpln.credentials import (
+    CplnClient,
+    CplnConfig,
+    CplnCredentials,
+)  # noqa F401
 from prefect_cpln.flows import run_namespaced_job  # noqa F401
 from prefect_cpln.jobs import CplnJob  # noqa F401
 from prefect_cpln.worker import CplnWorker  # noqa F401
